567884bc58639535134b4ccdf3dc8d57221187a4
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-27 12:14:39 +0000

    Fix typo in welcome message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Chào mừng đến với Nha Xe Blockchain!');
+    res.send('Chào mừng đến với Nhà Xe Blockchain!');
 });
 
 app.listen(PORT, () => {
