6d9c40b8061b707567d547a271bb533866209ff4
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-27 12:14:00 +0000

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Nền Tảng Quản Lý Xe Hơi Dựa Trên Blockchain</h1>
+      <p>Chào mừng đến với nền tảng của chúng tôi!</p>
+    </div>
+  );
+}
+
+export default App;
