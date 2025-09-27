05a9e5d23f7e8b5ae33a42780f6afca72e6cf564
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-27 12:15:17 +0000

    Refactor welcome message to include application name.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Blockchain Shuttle!');
+    res.send('Welcome to the Blockchain Shuttle Application!');
 });
 
 app.listen(PORT, () => {
