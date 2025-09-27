02d235a0710d5eb7e0ca0cf06a9f3c64f2f1868e
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-27 12:16:30 +0000

    Update welcome message for clarity

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Blockchain Shuttle Application!');
+    res.send('Welcome to the Blockchain Shuttle Application! Your gateway to decentralized solutions.');
 });
 
 app.listen(PORT, () => {
